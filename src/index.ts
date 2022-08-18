3d85f723b7d7ad4b270e74760b1842f1f2634cc4
zousongqi <zousongqi@huohua.cn>
2022-08-18 15:56:17 +0800

    feat: api registerComponent to registerFeature

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import React from 'react';
 import { IExtensionContext } from '@mtbird/shared';
 import Example from './components/Example';
 
 const activity = (context: IExtensionContext) => {
-  context.registerComponent('example.tab', Example);
+  context.registerFeature('example.tab', Example);
 };
 
 export default activity;
